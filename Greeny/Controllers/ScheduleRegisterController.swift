ffe4f5de8639f355eece08fd3e16311610de04c9
Ha Nguyen <thanhha.nguyen2211@gmail.com>
2018-05-15 15:18:48 +0700

    Update register schedule

--- Greeny/Controllers/ScheduleRegisterController.swift
+++ Greeny/Controllers/ScheduleRegisterController.swift
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 import CVCalendar
 
-class ScheduleRegisterController: UIViewController {
+class ScheduleRegisterController: UIViewController, CVCalendarViewDelegate, CVCalendarMenuViewDelegate {
     
     @IBOutlet weak var calendarMenuView: CVCalendarMenuView!
     @IBOutlet weak var calendarView: CVCalendarView!
@@ -18,8 +18,48 @@ class ScheduleRegisterController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        calendarMenuView.commitMenuViewUpdate()
-        calendarView.commitCalendarViewUpdate()
+        navigationController?.navigationBar.isHidden = false
+        navigationItem.title = "Schedule"
+        
+        calendarMenuView.dayOfWeekTextColor = .white
+        calendarMenuView.dayofWeekBackgroundColor = .white
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        calendarMenuView.commitMenuViewUpdate()
+        
+        calendarView.commitCalendarViewUpdate()
+        calendarView.backgroundColor = .white
+        
+    }
+    
+    func presentationMode() -> CalendarMode {
+        return CalendarMode.monthView
+    }
+    
+    func firstWeekday() -> Weekday {
+        return Weekday.monday
+    }
+    
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        return formatter
+    }()
+    
+    @IBAction func registerTapped(_ sender: UIButton) {
+        let date = calendarView.presentedDate.convertedDate()
+        let alert = UIAlertController(title: "Thông báo", message: "Đã đăng kí lịch nghỉ cho ngày \(dateFormatter.string(from: date!))", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: { (action) in
+            self.navigationController?.popViewController(animated: true)
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
+
+
+
+
+
